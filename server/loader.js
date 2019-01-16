a7c8a82a7458b4e156b1e27db4236ee3d7c67e1a
Brent Dykgraaf <brent.dykgraaf@aderant.com>
2019-01-16 17:06:35 +1300

    Tidy up core

--- server/loader.js
+++ server/loader.js
@@ -1,45 +1,45 @@
-import path from 'path';
-import fs from 'fs';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Helmet from 'react-helmet';
-import { Provider } from 'react-redux';
-import { StaticRouter } from 'react-router';
-import { Frontload, frontloadServerRender } from 'react-frontload';
-import Loadable from 'react-loadable';
+import path from "path";
+import fs from "fs";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import Helmet from "react-helmet";
+import { Provider } from "react-redux";
+import { StaticRouter } from "react-router";
+import { Frontload, frontloadServerRender } from "react-frontload";
+import Loadable from "react-loadable";
 
-import createStore from '../src/store';
-import App from '../src/app/app';
-import manifest from '../build/asset-manifest.json';
-import { setCurrentUser, logoutUser } from '../src/modules/auth';
+import createStore from "../src/store";
+import App from "../src/app/app";
+import manifest from "../build/asset-manifest.json";
+import { setCurrentUser, logoutUser } from "../src/modules/auth";
 
 export default (req, res) => {
   const injectHTML = (data, { html, title, meta, body, scripts, state }) => {
-    data = data.replace('<html>', `<html ${html}>`);
+    data = data.replace("<html>", `<html ${html}>`);
     data = data.replace(/<title>.*?<\/title>/g, title);
-    data = data.replace('</head>', `${meta}</head>`);
+    data = data.replace("</head>", `${meta}</head>`);
     data = data.replace(
       '<div id="root"></div>',
       `<div id="root">${body}</div><script>window.__PRELOADED_STATE__ = ${state}</script>`
     );
-    data = data.replace('</body>', scripts.join('') + '</body>');
+    data = data.replace("</body>", scripts.join("") + "</body>");
 
     return data;
   };
 
   fs.readFile(
-    path.resolve(__dirname, '../build/index.html'),
-    'utf8',
+    path.resolve(__dirname, "../build/index.html"),
+    "utf8",
     (err, htmlData) => {
       if (err) {
-        console.error('Read error', err);
+        console.error("Read error", err);
 
         return res.status(404).end();
       }
 
       const { store } = createStore(req.url);
 
-      if ('mywebsite' in req.cookies) {
+      if ("mywebsite" in req.cookies) {
         store.dispatch(setCurrentUser(req.cookies.mywebsite));
       } else {
         store.dispatch(logoutUser());
@@ -83,17 +83,18 @@ export default (req, res) => {
 
           res.end();
         } else {
-          // Otherwise, we carry on...
-
-          // Let's give ourself a function to load all our page-specific JS assets for code splitting
           const extractAssets = (assets, chunks) =>
             Object.keys(assets)
-              .filter(asset => chunks.indexOf(asset.replace('.js', '')) > -1)
+              .filter(asset => chunks.indexOf(asset.replace(".js", "")) > -1)
               .map(k => assets[k]);
 
           // Let's format those assets into pretty <script> tags
           const extraChunks = extractAssets(manifest, modules).map(
-            c => `<script type="text/javascript" src="/${c.replace(/^\//, '')}"></script>`
+            c =>
+              `<script type="text/javascript" src="/${c.replace(
+                /^\//,
+                ""
+              )}"></script>`
           );
 
           // We need to tell Helmet to compute the right meta tags, title, and such
@@ -101,7 +102,7 @@ export default (req, res) => {
 
           // NOTE: Disable if you desire
           // Let's output the title, just to see SSR is working as intended
-          console.log('THE TITLE', helmet.title.toString());
+          console.log("THE TITLE", helmet.title.toString());
 
           // Pass all this nonsense into our HTML formatting function above
           const html = injectHTML(htmlData, {
@@ -110,7 +111,7 @@ export default (req, res) => {
             meta: helmet.meta.toString(),
             body: routeMarkup,
             scripts: extraChunks,
-            state: JSON.stringify(store.getState()).replace(/</g, '\\u003c')
+            state: JSON.stringify(store.getState()).replace(/</g, "\\u003c")
           });
 
           // We have all the final HTML, let's send it to the user already!
